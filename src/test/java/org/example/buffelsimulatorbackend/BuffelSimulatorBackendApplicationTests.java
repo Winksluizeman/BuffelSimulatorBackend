cdbc6a13a206f4d542b239d5e316bc3d29f33f9d
WinkSluizeman53 <563017@student.fontys.nl>
2026-05-28 13:35:23 +0200

    Looking for the error in de ci/cd pipeline

--- src/test/java/org/example/buffelsimulatorbackend/BuffelSimulatorBackendApplicationTests.java
+++ src/test/java/org/example/buffelsimulatorbackend/BuffelSimulatorBackendApplicationTests.java
@@ -1,12 +1,12 @@
-//package org.example.buffelsimulatorbackend;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class BuffelSimulatorBackendApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//}
+package org.example.buffelsimulatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuffelSimulatorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
