ae37cf5edf82b47950d6cb9fee07684215adae21
WinkSluizeman53 <563017@student.fontys.nl>
2026-05-29 11:59:51 +0200

    Added versions to junit-jupiter and postgresql for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,8 +51,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:junit-jupiter:1.21.3")
+    testImplementation("org.testcontainers:postgresql:1.21.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
